1c80da4d0563ae1ab6c66a720c315fa530e43f24
Espen Grindhaug <espen.grindhaug@appeartv.com>
2023-12-10 12:54:54 +0100

    Solve 6b

--- AOC/P6.lean
+++ AOC/P6.lean
@@ -23,10 +23,59 @@ do
     tot := tot * numRecords
   println! "{tot}"
 
+def upperRecord' (min : Nat) (max : Nat) (timeout : Nat) (toBeat : Nat) : Nat :=
+  let guessFn := fun a b => (a + b) / 2
+  if min >= max then
+    max
+  else
+    let currentGuess := guessFn min max
+    let d := race currentGuess timeout
+    if d < toBeat then
+      upperRecord' min (currentGuess - 1) timeout toBeat
+    else
+      upperRecord' (currentGuess + 1) max timeout toBeat
+  termination_by upperRecord' a _ _ _ => a
+  decreasing_by { sorry }
+
+def upperRecord (timeout : Nat) (toBeat : Nat) (lower : Nat) : Nat :=
+  upperRecord' lower timeout timeout toBeat
+
+def lowerRecord' (min : Nat) (max : Nat) (timeout : Nat) (toBeat : Nat) : Nat :=
+  let guessFn := fun a b => (a + b) / 2
+  if min == max then
+    min + 1
+  else
+    let currentGuess := guessFn min max
+    let d := race currentGuess timeout
+    if d > toBeat then
+      lowerRecord' min (currentGuess - 1) timeout toBeat
+    else
+      lowerRecord' (currentGuess + 1) max timeout toBeat
+  termination_by lowerRecord' a _ _ _ => a
+  decreasing_by { sorry }
+
+def lowerRecord (timeout : Nat) (toBeat : Nat) (upper : Nat) : Nat :=
+  lowerRecord' 1 upper timeout toBeat
+
 def b (filename : String) : IO Unit :=
 do
   let f ← IO.FS.readFile filename
   let s := f.splitOn "\n" |>.dropLast
+
+  let time := s.get! 0 |>.splitOn ":" |>.get! 1 |>.replace " " "" |>.toNat!
+  let record := s.get! 1 |>.splitOn ":" |>.get! 1 |>.replace " " "" |>.toNat!
+
+  let firstGuess := time / 2
+  let d := race firstGuess time
+  if d < record then
+    println! "Failed to find a starting point!"
+    return ()
+
+  let lower := lowerRecord time record firstGuess
+  let upper := upperRecord time record firstGuess
+
+  let diff := upper - lower + 2
+  println! "{lower} - {upper} = {diff}"
 
 end P6
 end AOC
